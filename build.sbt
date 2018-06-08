4c9160926827535ac6f39fdf82be848cee0259fb
Dan Connolly <dckc@madmode.com>
2018-06-08 16:41:57 -0500

    node-client: showBlocks integration test
      - add circe-generic dependency

--- build.sbt
+++ build.sbt
@@ -187,6 +187,7 @@ lazy val nodeUI = (project in file("node-client"))
     libraryDependencies ++= Seq(
       "org.http4s"      %% "http4s-blaze-server" % Http4sVersion,
       "org.http4s"      %% "http4s-circe"        % Http4sVersion,
+      "io.circe"        %% "circe-generic"       % "0.6.1",
       "org.http4s"      %% "http4s-dsl"          % Http4sVersion,
       "org.specs2"      %% "specs2-core"         % Specs2Version % "test",
       "ch.qos.logback"  %  "logback-classic"     % LogbackVersion
--- node-client/src/main/scala/coop/rchain/rnodeclient/HelloWorldService.scala
+++ node-client/src/main/scala/coop/rchain/rnodeclient/HelloWorldService.scala
@@ -4,16 +4,27 @@ import cats.effect.Effect
 import io.circe.Json
 import org.http4s.HttpService
 import org.http4s.circe._
+import io.circe.{Encoder}
+import io.circe.syntax._
+import io.circe.generic.semiauto._
 import org.http4s.dsl.Http4sDsl
 
 import coop.rchain.node.model.diagnostics.{DiagnosticsGrpc}
 import coop.rchain.node.model.repl.{EvalRequest, ReplGrpc, ReplResponse}
-import coop.rchain.casper.protocol.{BlockMessage, BlockQuery, DeployServiceGrpc, DeployString}
+import coop.rchain.casper.protocol.{
+  BlockInfo,
+  BlockMessage,
+  BlockQuery,
+  DeployServiceGrpc,
+  DeployString
+}
 
 import com.google.protobuf.empty.Empty
 import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 
 class HelloWorldService[F[_]: Effect] extends Http4sDsl[F] {
+
+  implicit val blockInfoEncoder: Encoder[BlockInfo] = deriveEncoder[BlockInfo]
 
   def makeChannel(): ManagedChannel = {
     // ISSUE: when to build the channel? not every time, surely
@@ -24,19 +35,29 @@ class HelloWorldService[F[_]: Effect] extends Http4sDsl[F] {
 
   val service: HttpService[F] = {
     HttpService[F] {
-      case GET -> Root => {
+      case GET -> Root / "blocks" => {
+        val blockingStub = DeployServiceGrpc.blockingStub(makeChannel())
+        val blocks       = blockingStub.showBlocks(Empty()).blocks
+
+        Ok(Json.obj("blocks" -> blocks.map { blockInfoEncoder(_) }.toList.asJson))
+      }
+
+      case GET -> Root / "peers" => {
         val blockingStub = DiagnosticsGrpc.blockingStub(makeChannel())
         val peers        = blockingStub.listPeers(Empty()).peers
 
         Ok(Json.obj("peers" -> Json.fromInt(peers.size)))
       }
-      case GET -> Root / "hello" / name =>
-        Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
-      case GET -> Root / "eval" / program =>
+
+      case GET -> Root / "eval" / program => {
         val blockingStub = ReplGrpc.blockingStub(makeChannel())
         val output       = blockingStub.eval(EvalRequest(program)).output
 
         Ok(Json.obj("output" -> Json.fromString(output)))
+      }
+
+      case GET -> Root / "hello" / name =>
+        Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
     }
   }
 }

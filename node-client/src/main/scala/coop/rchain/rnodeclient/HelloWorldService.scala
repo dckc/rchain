73701d946b584106a92cc03d0e01043777b3d296
Dan Connolly <dckc@madmode.com>
2018-06-08 15:31:21 -0500

    node-client: eval integration test working

--- node-client/src/main/scala/coop/rchain/rnodeclient/HelloWorldService.scala
+++ node-client/src/main/scala/coop/rchain/rnodeclient/HelloWorldService.scala
@@ -6,7 +6,8 @@ import org.http4s.HttpService
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
-import coop.rchain.node.model.diagnostics.DiagnosticsGrpc
+import coop.rchain.node.model.diagnostics.{DiagnosticsGrpc}
+import coop.rchain.node.model.repl.{EvalRequest, ReplGrpc, ReplResponse}
 import coop.rchain.casper.protocol.{BlockMessage, BlockQuery, DeployServiceGrpc, DeployString}
 
 import com.google.protobuf.empty.Empty
@@ -14,20 +15,28 @@ import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 
 class HelloWorldService[F[_]: Effect] extends Http4sDsl[F] {
 
+  def makeChannel(): ManagedChannel = {
+    // ISSUE: when to build the channel? not every time, surely
+    val host = "0.0.0.0" // ISSUE: config?
+    val port = 50000     // ISSUE: config?
+    return ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build
+  }
+
   val service: HttpService[F] = {
     HttpService[F] {
       case GET -> Root => {
-        val host = "0.0.0.0" // ISSUE: config?
-        val port = 50000     // ISSUE: config?
-        // ISSUE: when to build the channel? not every time, surely
-        val channel      = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build
-        val blockingStub = DiagnosticsGrpc.blockingStub(channel)
+        val blockingStub = DiagnosticsGrpc.blockingStub(makeChannel())
         val peers        = blockingStub.listPeers(Empty()).peers
 
         Ok(Json.obj("peers" -> Json.fromInt(peers.size)))
       }
       case GET -> Root / "hello" / name =>
         Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+      case GET -> Root / "eval" / program =>
+        val blockingStub = ReplGrpc.blockingStub(makeChannel())
+        val output       = blockingStub.eval(EvalRequest(program)).output
+
+        Ok(Json.obj("output" -> Json.fromString(output)))
     }
   }
 }
